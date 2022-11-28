c9625dfb3e43a601cb45aa2ebd3477612339a61f
Ghost <108669382+Wemubis@users.noreply.github.com>
2022-11-28 01:41:30 +0100

    Add files via upload

--- Makefile
+++ Makefile
@@ -1,4 +1,3 @@
-
 # makefile pour ft_printf, need to add the one for libc
 NAME = libftprintf.a
 
@@ -8,18 +7,9 @@ AR = ar
 ARFLAGS = rcs
 RM = rm -f
 
-SRCS = ft_atoi.c ft_bzero.c ft_calloc.c ft_isalnum.c \
-	ft_isalpha.c ft_isdigit.c ft_isprint.c ft_isascii.c \
-	ft_memchr.c ft_memcmp.c ft_memcpy.c ft_memmove.c \
-	ft_memset.c ft_strchr.c ft_strdup.c ft_strlcat.c \
-	ft_strlcpy.c ft_strlen.c ft_strncmp.c ft_strnstr.c \
-	ft_strrchr.c ft_tolower.c ft_toupper.c ft_substr.c \
-	ft_strjoin.c ft_strtrim.c ft_split.c ft_itoa.c \
-	ft_strmapi.c ft_striteri.c ft_putchar_fd.c ft_putstr_fd.c \
-	ft_putendl_fd.c ft_putnbr_fd.c \
+SRCS = ft_printf.c \
 
-
-HEADER = libft.h
+HEADER = ft_printf.h
 OBJS =  $(patsubst %.c,%.o,$(SRCS))
 
 
--- ft_printf.h
+++ ft_printf.h
@@ -1,32 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mle-boud <mle-boud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/21 14:49:08 by mle-boud          #+#    #+#             */
-/*   Updated: 2022/11/28 01:23:47 by mle-boud         ###   ########.fr       */
+/*   Created: 2022/11/07 13:44:52 by mle-boud          #+#    #+#             */
+/*   Updated: 2022/11/28 01:31:58 by mle-boud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include"ft_printf.h"
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
 
-char	*choose_conv(char *str)
-{}
+/*-------------------------HEAD-------------------------*/
 
-int	ft_printf(const char *format, ...)
-{
-	va_list	to_print;
-	char	*select;
-	int		ret;
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdint.h>
 
-	va_start(to_print, format);
-	// ret = ;
-	va_end(to_print);
-	return (ret);
-}
+/*-------------------------FCTN-------------------------*/
 
-// Mission
-// Create file to convert the sentences
-// Create function to chose which converstion use
+/*---------------CORE---------------*/
+int	ft_printf(const char *format, ...);
+
+#endif
